9bebdcade6cccbed4fd791ff995c8c0ada895615
WFH <loving23aa@hotmail.com>
2019-10-11 19:20:46 +0800

    添加前端html页面并使用nginx联调

--- src/route/blog.js
+++ src/route/blog.js
@@ -16,8 +16,20 @@ const handleBlogRouter = (req, res) => {
 
   // 获取博客列表
   if (req.method === 'GET' && req.path === '/api/blog/list') {
-    const author = req.query.author || '';
+    let author = req.query.author || '';
     const keyword = req.query.keyword || '';
+
+    if (req.query.isadmin) {
+      // 管理员界面
+      const loginCheckResult = loginCheck(req);
+      if (loginCheckResult) {
+        // 未登录
+        return loginCheckResult;
+      }
+      // 强制查询自己的博客
+      author = req.session.username;
+    }
+
     const result = getList(author, keyword);
 
     return result.then((listdata) => {
@@ -49,7 +61,7 @@ const handleBlogRouter = (req, res) => {
     const loginCheckResult = loginCheck(req);
     if (loginCheckResult) {
       // 未登录
-      return loginCheck;
+      return loginCheckResult;
     }
 
     req.body.author = req.session.username;
@@ -61,7 +73,7 @@ const handleBlogRouter = (req, res) => {
   if (req.method === 'POST' && req.path === '/api/blog/update') {
     const loginCheckResult = loginCheck(req);
     if (loginCheckResult) {
-      return loginCheck;
+      return loginCheckResult;
     }
 
     req.body.author = req.session.username;
@@ -79,7 +91,7 @@ const handleBlogRouter = (req, res) => {
   if (req.method === 'POST' && req.path === '/api/blog/del') {
     const loginCheckResult = loginCheck(req);
     if (loginCheckResult) {
-      return loginCheck;
+      return loginCheckResult;
     }
 
     req.body.author = req.session.username;
